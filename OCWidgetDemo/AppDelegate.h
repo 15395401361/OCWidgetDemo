50abbcad741c02216d3636d7a951ed19cce5f6da
吴林丰 <hanshanshi@7lk.com>
2017-04-24 15:12:22 +0800

    Initial Commit

---
+++ OCWidgetDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCWidgetDemo
+//
+//  Created by 吴林丰 on 2017/4/24.
+//  Copyright © 2017年 吴林丰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
